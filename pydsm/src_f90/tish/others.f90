8c6efbed6d78e04c9afeef35071b04e0ba6c3177
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-05-18 17:26:28 +0800

    bye pinput2

--- pydsm/src_f90/tish/others.f90
+++ pydsm/src_f90/tish/others.f90
@@ -1,85 +1,3 @@
-subroutine pinput2(parameter_file, maxnlay,maxnzone,maxnr, &
-      re,ratc,ratl,tlen,np,omegai,imin,imax, &
-      nzone,vrmin,vrmax,rho,vsv,vsh,qmu, &
-      r0,eqlat,eqlon,mt,nr,theta,phi,lat,lon,output)
-!------------------------------------------------------------------------
-! Parameter Input
-!------------------------------------------------------------------------
-      implicit none
-
-      integer, parameter :: MAX_LINES = 10000
-      character*160, intent(in) :: parameter_file
-      integer, intent(in) :: maxnlay,maxnzone,maxnr
-      integer, intent(out) :: np
-      integer, intent(out) :: imin,imax
-      integer, intent(out) :: nzone,nr
-      real*8, intent(out) :: tlen,omegai,re,ratc,ratl
-      real*8, dimension(maxnzone), intent(out) :: vrmin,vrmax,qmu
-      real*8, dimension(4,maxnzone), intent(out) :: rho,vsv,vsh
-      real*8, intent(out) :: r0,mt(3,3)
-      real*8, dimension(maxnr), intent(out) :: theta,phi,lat,lon
-      real*8, intent(out) :: eqlat,eqlon
-      character*80, dimension(maxnr), intent(out) :: output
-      real*8 :: stlat,stlon,eqlattmp
-      integer i,j,io
-      character*160 :: line
-      character*160, dimension(MAX_LINES) :: lines
-!
-! reading the parameter file
-      j = 0
-      io = 0
-      open(unit=11, file=parameter_file, status='old',action='read')
-      do while (io > 0)
-       read(11, '(a160)', iostat=io) line
-      	if (line(1:1) /= 'c' .and. line(1:3) /= 'end') then
-                  lines(j) = line
-                  j = j + 1
-      	endif
-      enddo
-
-      read(lines(1),*) tlen,np
-      read(lines(2),*) re		! relative error (vertical grid)
-      read(lines(3),*) ratc		! ampratio (vertical grid cut-off)
-      read(lines(4),*) ratl		! ampratio (for l-cutoff)
-      read(lines(5),*) omegai	! omegai
-      omegai = - dlog(omegai) / tlen
-!
-      read(lines(6),*) imin,imax
-      read(lines(7),*) nzone
-      if (nzone.gt.maxnzone) then
-            write(*,*) 'nzone is too large. (pinput)'
-            return
-      endif
-! structure
-      do i=1,nzone
-         read(lines(7+i),*) vrmin(i),vrmax(i), &
-     	             rho(1,i),rho(2,i),rho(3,i),rho(4,i), &
-     	             vsv(1,i), vsv(2,i), vsv(3,i), vsv(4,i), &
-     	             vsh(1,i), vsh(2,i), vsh(3,i), vsh(4,i), qmu(i)
-      enddo
-! source parameter
-      read(lines(nzone+8),*) r0,eqlat,eqlon
-      eqlattmp = eqlat
-      call translat(eqlat,eqlattmp)
-      read(lines(nzone+9),*) mt(1,1),mt(1,2),mt(1,3),mt(2,2),mt(2,3),mt(3,3)
-      read(lines(nzone+10),*) nr
-      if (nr > maxnr) then
-      	write(*,*) 'nr is too large. (pinput)'
-      	return
-      endif
-      do i=1,nr
-      	read(lines(nzone+10+i),*) lat(i),lon(i)
-      	stlat = lat(i)
-      	stlon = lon(i)
-      	call translat(lat(i),stlat)
-      	call calthetaphi(eqlattmp,eqlon,stlat,stlon,theta(i),phi(i))
-      enddo
-      do i=1,nr
-      	read(lines(nzone+10+nr+i),'(a80)') output(i)
-      enddo
-!
-      return
-      end
 !------------------------------------------------------------------------
 subroutine pinput_fromfile(parameter_file, &
       re,ratc,ratl,tlen,np,omegai,imin,imax, &
@@ -171,7 +89,7 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
 !------------------------------------------------------------------------
       use parameters
       implicit none
-!	
+!
       real(dp), intent(in) :: ievla,ievlo,istla,istlo
       real(dp), intent(out) :: theta,phi
       real(dp) evla,evlo,stla,stlo
@@ -187,7 +105,7 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
       evlo = ievlo / 1.8d2 * pi
       stla = stla / 1.8d2 * pi
       stlo = istlo / 1.8d2 * pi
-!  
+!
       gcarc = dacos( dcos(evla) * dcos(stla) &
           + dsin(evla) * dsin(stla) * dcos(evlo - stlo) )
 !
@@ -668,16 +586,16 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
 !------------------------------------------------------------------------
       integer n1,n2,i,j
       complex*16 a(n1,*)
-      
+
       do 110 j=1,n2
       	do 100 i=1,n1
       	a(i,j) = dcmplx( 0.d0 )
       100	  continue
       110	continue
-      
+
       return
       end
-      
+
 !------------------------------------------------------------------------
       subroutine cvecinit( nn,b )
 !------------------------------------------------------------------------
@@ -685,14 +603,14 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
 !------------------------------------------------------------------------
       integer nn,i
       complex*16 b(*)
-      
+
       do 100 i=1,nn
       	b(i) = dcmplx( 0.d0 )
       100	continue
-      
+
       return
       end
-      
+
 !------------------------------------------------------------------------
       subroutine calcutd(nzone,nnl,tmpr,rat,nn,ra,kc)
 !------------------------------------------------------------------------
@@ -702,34 +620,34 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
       complex*16 tmpr(*)
       real*8 rat,ra(*)
       integer nc
-      
+
       real*8 cU(nn),rc
       real*8 maxamp,amp(nn)
       integer iz,jz,jj,i,ml(nzone),tzone
-      
+
       do 100 jj=1,nn
       	cU(jj) = 0.d0
       100	continue
-      
+
       iz = 2
       jz = 1
       do 110 jj=1,nn
 !	   cU(jj) = cdabs(tmpc(jj))
       	cU(jj) = tmpr(jj)
       110	continue
-      
+
       maxamp = -1.d0
       do 120 i=1,nn
       	amp(i) = cU(i)
       	if(maxamp.lt.amp(i)) maxamp = amp(i)
       120	continue
-      
+
       maxamp = maxamp * rat ! threshold value
       if(maxamp.eq.0.d0) then
       	kc = 1
       	return
       endif
-      
+
       do 130 i=1,nn
       	if(amp(i).gt.maxamp) then
       		nc = i
@@ -737,48 +655,48 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
       	endif
       130	continue
       140	continue
-      
+
       i = 1
       do 150 jj=1,nzone
       	i = i + nnl(jj)
       	ml(jj) = i
       150	continue
-      
+
       do 160 jj=nzone,1,-1
       	if(ml(jj).gt.nc) tzone = jj
       160	continue
-      
+
       rc = ra(nc)
       kc = nc
-      
-      
+
+
       return
       end
-      
+
 !------------------------------------------------------------------------
       subroutine callsuf(omega,nzone,vrmax,vsv,lsuf)
 !------------------------------------------------------------------------
       implicit none
       integer nzone,lsuf
       real*8 omega,vrmax(*),vsv(4,*)
-      
+
       real*8 tvs,coef
       integer i
-      
+
       tvs = 0.d0
       do 100 i=1,4
       	if(i.eq.1) then
       		coef = 1.d0
       	else
-      		coef = coef 
+      		coef = coef
       	endif
       	tvs = tvs + ( vsv(i,nzone) ) * coef
       100	continue
-      
+
       lsuf = int(omega * vrmax(nzone) / tvs - 0.5d0) + 1
       return
       end
-      
+
 !------------------------------------------------------------------------
       subroutine calamp(g,l,lsuf,maxamp,ismall,ratl)
 !------------------------------------------------------------------------
@@ -786,9 +704,9 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
       integer l,lsuf,ismall
       real*8 maxamp,ratl
       complex*16 g
-      
+
       real*8 amp,ampratio
-      
+
       ampratio = 0.d0
       amp = cdabs(g)
       if( amp.gt.maxamp ) maxamp = amp
@@ -800,6 +718,6 @@ subroutine calthetaphi(ievla,ievlo,istla,istlo,theta,phi)
       else
       	ismall = 0
       endif
-      
+
       return
       end
