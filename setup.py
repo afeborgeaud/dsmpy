3816517ecb8e674084a67b79b9591cda4ce3c264
anselme <anselme@earth.sinica.edu.tw>
2020-12-11 16:22:48 +0800

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import setuptools
 from numpy.distutils.core import setup, Extension
 
 lib_tish = Extension(
-    name='dsmpy.flib.tish',
-    sources=[
-        'dsmpy/src_f90/tish/parameters.f90',
-        'dsmpy/src_f90/tish/tish.f90',
-        'dsmpy/src_f90/tish/others.f90',
-        'dsmpy/src_f90/tish/calmat.f90',
-        'dsmpy/src_f90/tish/trialf.f90',
-        'dsmpy/src_f90/tish/dclisb.f90'
-    ],
-    extra_f90_compile_args=['-Ofast'],
-    extra_f77_compile_args=['-Ofast'],
-)
+        name='dsmpy.flib.tish',
+        sources=[
+            'dsmpy/src_f90/tish/parameters.f90',
+            'dsmpy/src_f90/tish/tish.f90',
+            'dsmpy/src_f90/tish/others.f90',
+            'dsmpy/src_f90/tish/calmat.f90',
+            'dsmpy/src_f90/tish/trialf.f90',
+            'dsmpy/src_f90/tish/dclisb.f90'
+        ],
+        extra_f90_compile_args=['-Ofast'],
+        extra_f77_compile_args=['-Ofast'],
+    )
 
 lib_tipsv = Extension(
     name='dsmpy.flib.tipsv',
@@ -31,43 +31,39 @@ lib_tipsv = Extension(
     extra_f77_compile_args=['-Ofast'],
 )
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+if __name__ == '__main__':
+    with open('README.md', 'r') as fh:
+        long_description = fh.read()
 
-setup(
-    name="dsmpy",
-    version='1.0a1',
-    author='Anselme Borgeaud, Kensuke Konishi',
-    author_email='aborgeaud@gmail.com',
-    license='MIT',
-    description='Python wrapper for DSM',
-    long_description = long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/afeborgeaud/dsmpy',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'obspy',
-        'numpy',
-        'mpi4py',
-        'pandas',
-        'matplotlib',
-        'geographiclib',
-    ],
-    ext_modules=[lib_tish, lib_tipsv],
-    python_requires='>=3.7',
-    package_data={
-        'dsmpy' : ['resources/scardec.pkl'],
-    },
-    entry_points={
-        'console_scripts': [
-            'dsmpy=dsmpy.commandline:main',
+    setup(
+        name="dsmpy",
+        version='1.0a1',
+        author='Anselme Borgeaud, Kensuke Konishi',
+        author_email='aborgeaud@gmail.com',
+        license='MIT',
+        description='Python wrapper for DSM',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/afeborgeaud/dsmpy',
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
         ],
-    },
-)
+        install_requires=[
+            'obspy',
+            'mpi4py',
+            'pandas',
+            'matplotlib',
+            'geographiclib',
+            "numpy",
+        ],
+        ext_modules=[lib_tish, lib_tipsv],
+        python_requires='>=3.7',
+        package_data={
+            'dsmpy' : ['resources/scardec.pkl'],
+        },
+    )
