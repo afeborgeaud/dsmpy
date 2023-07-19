836d8b6683569b27ccb1bef429a6a9c82d4ef4dd
Nobuaki Fuji <seismobassoon@gmail.com>
2023-07-19 16:23:42 +0200

    Event type

--- dsmpy/utils/cmtcatalog.py
+++ dsmpy/utils/cmtcatalog.py
@@ -11,7 +11,7 @@ import requests
 def convert_catalog(cat):
     #cat = read_events(root_resources + 'gcmt.ndk')
     #mts = np.zeros((cat.count(), 6), dtype=np.float32)
-    events = np.empty(cat.count(), dtype=object)
+    events = np.empty(shape=cat.count(),dtype=Event)
     for i, event in enumerate(cat):
         tensor = event.preferred_focal_mechanism().moment_tensor.tensor
         mw = [m for m in event.magnitudes 
